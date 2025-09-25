6af602ae7cc916e49740e12ce4343cafa8288315
Aydin324 <ayddin2004@gmail.com>
2025-09-25 18:12:40 +0200

    Modified FactService to be able to fetch all facts using separate interface. In facts-list script added methods for the service and pagination

--- src/app/pages/facts-list/facts-list.ts
+++ src/app/pages/facts-list/facts-list.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CatFact, FactService } from '../../services/fact';
 
 @Component({
   selector: 'app-facts-list',
@@ -11,8 +12,36 @@ import { FormsModule } from '@angular/forms';
 export class FactsList {
   name: string = '';
   submitted = false;
+  current_page = 1;
+  last_page = 0;
+  facts: CatFact[] = [];
+
+  constructor(private factService: FactService) {}
 
   submitName() {
     this.submitted = true;
+    this.loadFacts(1);
+  }
+
+  loadFacts(page: number) {
+    this.factService.getFacts(page).subscribe((response) => {
+      this.facts = response.data;
+      this.last_page = response.last_page;
+      this.current_page = response.current_page;
+    });
+  }
+
+  nextPage() {
+    if (this.current_page < this.last_page) {
+      this.current_page++;
+      this.loadFacts(this.current_page);
+    }
+  }
+
+  previousPage() {
+    if (this.current_page > 1) {
+      this.current_page--;
+      this.loadFacts(this.current_page);
+    }
   }
 }
--- src/app/services/fact.ts
+++ src/app/services/fact.ts
@@ -6,16 +6,29 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FactService {
-  private api = 'https://catfact.ninja/fact';
+  private fact_api = 'https://catfact.ninja/fact';
+  private facts_api = 'https://catfact.ninja/facts?page=';
 
   constructor(private http: HttpClient) {}
 
   getFact(): Observable<CatFact> {
-    return this.http.get<CatFact>(this.api);
+    return this.http.get<CatFact>(this.fact_api);
+  }
+
+  getFacts(page: number): Observable<AllCatFacts> {
+    return this.http.get<AllCatFacts>(this.facts_api+page);
   }
 }
 
 export interface CatFact {
   fact: string;
   length?: number;  
+}
+
+export interface AllCatFacts {
+  current_page: number;
+  data: CatFact[];
+  last_page: number;
+  per_page: number;
+  total: number;
 }
